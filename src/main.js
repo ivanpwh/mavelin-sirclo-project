409265442d653411e34a5fdf9e535f0521ee26b2
ivan putra widyatama hukom <ivanpwh@gmail.com>
2020-04-19 01:03:08 +0700

    Set router and master components

--- src/main.js
+++ src/main.js
@@ -3,13 +3,23 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import BootstrapVue from 'bootstrap-vue'
+import store from './store'
+import axios from 'axios'
+
+// handle cors issue
+const BASE_URL = 'https://cors-anywhere.herokuapp.com/https://files.sirclocdn.xyz/frontend-test-37'
+
+axios.defaults.baseURL = BASE_URL
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
